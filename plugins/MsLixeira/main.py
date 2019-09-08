55fed74ea043a7d3820b786fbb873f99afdf7563
yurirp4 <yuridasilvaribeiro2014@gmail.com>
2019-09-08 18:31:23 -0300

    Create main.py

---
+++ plugins/MsLixeira/main.py
@@ -0,0 +1,33 @@
+![s](https://i.imgur.com/jZTy96d.png)
+
+![s](https://i.imgur.com/Td7EsNZ.png)
+
+-  /lixeira
+
+- /lixo
+
+- /trash
+
+
+![s](https://i.imgur.com/ZJCVVIj.png)
+
+- MsLixeira.usar
+
+![s](https://i.imgur.com/OAtkQHR.png)
+
+![s](https://image.prntscr.com/image/RI9nWieSR9e18V-rJnKNtQ.png)
+![s](https://image.prntscr.com/image/uKYdzb6FT8eDRYoyyHWHAg.png)
+![s](https://image.prntscr.com/image/GuysOQp7RCCwA8ooX_avMg.png)
+![s](https://image.prntscr.com/image/Dh2MpAO0Tee5FTVR3w3C8A.png)
+![s](https://image.prntscr.com/image/LksfoyUhSfWBj-FWn99_Eg.png)
+![s](https://image.prntscr.com/image/FosNCr1JRbiOUis-Xa40_g.png)
+![s](https://image.prntscr.com/image/M46GFa2xSnacC9xCEDj1dw.png)
+![s](https://image.prntscr.com/image/VTc8XTB_REizoGgPJov3jA.png)
+![s](https://image.prntscr.com/image/l5HNnk2yRo2ty_YgriFuUw.png)
+
+
+
+![s](https://i.imgur.com/cqS8qkN.png)
+- o Plugin tem 2 Opções 1º você abre a lixeira onde pode apenas colocar seu itens e fecha e 2º Limpa Todo seu Inventario , Mais para limpar o inventario antes você passa por uma  confirmação para ter certeza que  você  deseja Limpar o Inventario
+
+- Ideias  de plugin , Ideias para o Plugin , bugs ? email: mcskypvpsuport@gmail.com
